ea182008782aed4d1ecaabea3cb1edda59629125
nu4pet <chen.huang@nu4pet.com>
2021-03-16 11:58:33 +0800

    fix: app/router

--- app/router/index.js
+++ app/router/index.js
@@ -1,14 +1,5 @@
 /* eslint-disable no-unused-vars */
 const route = require('./route')
-<<<<<<< HEAD
-//const ListModel = require('../../src/js/models/data/list-model')
-//const ProdModel = require('../../src/js/models/data/prod')
-//const PetModel = require('../../src/js/models/data/pet')
-//const UserModel = require('../../src/js/models/data/user')
-//const ShopCarModel = require('../../src/js/models/data/shop_car')
-//const RecordModel = require('../../src/js/models/data/record')
-//const GiveawayModel = require('../../src/js/models/data/giveaway')
-=======
 // const ListModel = require('../../src/js/models/data/list-model')
 // const ProdModel = require('../../src/js/models/data/prod')
 // const PetModel = require('../../src/js/models/data/pet')
@@ -16,7 +7,6 @@ const route = require('./route')
 // const ShopCarModel = require('../../src/js/models/data/shop_car')
 // const RecordModel = require('../../src/js/models/data/record')
 // const GiveawayModel = require('../../src/js/models/data/giveaway')
->>>>>>> 91f625d677d2ef457f09a9251451328991920706
 
 module.exports = (args)=>{
     const Route = new route(args)
